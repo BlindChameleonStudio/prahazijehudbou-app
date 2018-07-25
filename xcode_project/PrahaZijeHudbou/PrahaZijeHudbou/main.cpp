37c3040609fbc9f6d1201b0c9ba886de2154985f
cernyfilip <filipcerny1989@gmail.com>
2018-07-26 00:48:38 +0200

    added gitignore file, djinji, c++ crossplatform structure, xcode project

---
+++ xcode_project/PrahaZijeHudbou/PrahaZijeHudbou/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PrahaZijeHudbou
+//
+//  Created by Filip Cerny on 26/07/2018.
+//  Copyright © 2018 BlindChameleonStudio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
