289d755e1ed3874b1b7ba9028a22bc67531eea85
cernyfilip <filipcerny1989@gmail.com>
2018-07-26 02:25:17 +0200

    added iOS app, connected to c++ methods

---
+++ xcode_project/PrahaZijeHudbou/Praha Zije Hudbou iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Praha Zije Hudbou iOS
+//
+//  Created by Filip Cerny on 26/07/2018.
+//  Copyright © 2018 BlindChameleonStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
--- xcode_project/PrahaZijeHudbou/PrahaZijeHudbou/main.cpp
+++ xcode_project/PrahaZijeHudbou/PrahaZijeHudbou/main.cpp
@@ -7,9 +7,15 @@
 //
 
 #include <iostream>
+#include "hello_world_impl.hpp"
+
+using namespace std;
+using namespace helloworld;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    HelloWorldImpl hw = HelloWorldImpl();
+    string myString = hw.get_hello_world();
+    cout << myString << "\n";
     return 0;
 }
+
